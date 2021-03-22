57373100acfa2dcb2f72637738b54e33c2128712
kurtKostein <74211778+kurtKostein@users.noreply.github.com>
2021-03-22 22:53:22 +0300

    Model skeleton (#3)
    * update requirements.txt
    
    install djangoframework-simplejwt and freeze > requirements.txt
    
    * create api_v1 app
    
    create skeleton for models.py

---
+++ api_v1/models.py
@@ -0,0 +1,21 @@
+from django.db import models
+
+
+class Titles(models.Model):
+    pass
+
+
+class Categories(models.Model):
+    pass
+
+
+class Genres(models.Model):
+    pass
+
+
+class Reviews(models.Model):
+    pass
+
+
+class Comments(models.Model):
+    pass
