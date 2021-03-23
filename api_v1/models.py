5f7de42a7b30a1678aa50b2ef1d0b6a928aa772b
Farank1555 <beschastnovia@yandex.ru>
2021-03-24 00:43:56 +0300

    test ivan git

--- api_v1/models.py
+++ api_v1/models.py
@@ -1,5 +1,6 @@
 from django.db import models
-
+from django.contrib.auth.models import AbstractUser
+from django.db import models
 
 class Titles(models.Model):
     pass
@@ -19,3 +20,35 @@ class Reviews(models.Model):
 
 class Comments(models.Model):
     pass
+
+class User(AbstractUser):
+
+# ПОЛЯ
+    EMAIL_FIELD = 'email'
+    USER_FIELDS = [] # 'username'
+    USER_ROLES = [
+        ('admin', 'Администратор'),
+        ('moderator', 'Модератор'),
+        ('user', 'Пользователь'),
+    ]
+
+    email = models.EmailField(unique=True)
+    confirmation_code = models.CharField(
+        max_length=36,
+        blank=True,
+        unique=True
+    )
+    role = models.CharField(
+        max_length=36,
+        choices=USER_ROLES,
+        default='user'
+    )
+    bio = models.TextField(max_length=300, blank=True)
+    #
+    @property
+    def is_admin(self):
+        return self.role == 'admin' or self.is_staff
+
+    @property
+    def is_moderator(self):
+        return self.role == 'moderator'
