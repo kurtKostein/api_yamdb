c4033a9e108f5c9cb60fec6d9f5f6fea1a290413
Farank1555 <74375757+Farank1555@users.noreply.github.com>
2021-03-24 00:50:34 +0300

    test ivan git (#5)
    Co-authored-by: kurtKostein <74211778+kurtKostein@users.noreply.github.com>

--- api_v1/models.py
+++ api_v1/models.py
@@ -1,5 +1,37 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+from django.db import models
 
+
+class User(AbstractUser):
+    EMAIL_FIELD = 'email'
+    USER_FIELDS = [] # 'username'
+    USER_ROLES = [
+        ('admin', 'Администратор'),
+        ('moderator', 'Модератор'),
+        ('user', 'Пользователь'),
+    ]
+
+    email = models.EmailField(unique=True)
+    confirmation_code = models.CharField(
+        max_length=36,
+        blank=True,
+        unique=True
+    )
+    role = models.CharField(
+        max_length=36,
+        choices=USER_ROLES,
+        default='user'
+    )
+    bio = models.TextField(max_length=300, blank=True)
+    
+    @property
+    def is_admin(self):
+        return self.role == 'admin' or self.is_staff
+
+    @property
+    def is_moderator(self):
+        return self.role == 'moderator'
 
 class Titles(models.Model):
     name = models.CharField(max_length=200)
