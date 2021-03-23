6de6cd8ec972a472035595c5d76de2a5c91cfeae
Angelina Myagkova <74258829+myagkova@users.noreply.github.com>
2021-03-23 22:36:33 +0100

    Angelina/update models (#4)
    * add titles, genres, categories models
    
    * update models

--- api_v1/models.py
+++ api_v1/models.py
@@ -2,15 +2,25 @@ from django.db import models
 
 
 class Titles(models.Model):
-    pass
+    name = models.CharField(max_length=200)
+    year = models.IntegerField()
+    description = models.TextField()
+    genre = models.ForeignKey(
+        Genres, models.SET_NULL, related_name='title_genre'
+    )
+    category = models.ForeignKey(
+        Categories, models.SET_NULL, related_name='title_category'
+    )
 
 
 class Categories(models.Model):
-    pass
+    name = models.CharField(max_length=200)
+    slug = model.CharField(max_length=200)
 
 
 class Genres(models.Model):
-    pass
+    name = models.CharField(max_length=200)
+    slug = model.CharField(max_length=200)
 
 
 class Reviews(models.Model):
