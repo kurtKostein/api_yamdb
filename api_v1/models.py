05bebe1c430f0472bf07bae6e2e5ba963fcd1a71
kurtkostein <kurtkostein@yandex.ru>
2021-03-24 07:19:12 +0300

    my models in models.py

--- api_v1/models.py
+++ api_v1/models.py
@@ -1,21 +1,36 @@
 from django.db import models
 
 
-class Titles(models.Model):
+class Title(models.Model):
     pass
 
 
-class Categories(models.Model):
+class Category(models.Model):
     pass
 
 
-class Genres(models.Model):
+class Genre(models.Model):
     pass
 
 
-class Reviews(models.Model):
-    pass
+class Review(models.Model):  # TODO
+    title_id = models.ForeignKey(to='Title',
+                                 related_name='reviews',
+                                 on_delete=models.CASCADE)
+    text = models.TextField()
+    author = models.ForeignKey(to='User',
+                               related_name='reviews',
+                               on_delete=models.CASCADE)
+    score = models.PositiveIntegerField(null=True, blank=True)
+    pub_date = models.DateTimeField(auto_now_add=True)
 
 
-class Comments(models.Model):
-    pass
+class Comment(models.Model):
+    review_id = models.ForeignKey(to='Review',
+                                  related_name='reviews',
+                                  on_delete=models.CASCADE)
+    text = models.TextField()
+    author = models.ForeignKey(to='User',
+                               related_name='reviews',
+                               on_delete=models.CASCADE)
+    pub_date = models.DateTimeField(auto_now_add=True)
