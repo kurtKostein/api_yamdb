c2f5046b0b5c7b867fc6bbf8fd9cc720aba1264e
kurtkostein <kurtkostein@yandex.ru>
2021-03-24 09:17:11 +0300

    refactor serializer.py
    add skeleton for user, review and comment serializer

--- api_v1/serializer.py
+++ api_v1/serializer.py
@@ -1,24 +1,45 @@
 from rest_framework import serializers
 
-from .models import Categories, Genres, Titles
+from .models import User, Category, Genre, Title, Review, Comment
 
 
-class TitlesSerializer(serializers.ModelSerializer):
-
-    class Meta:
-        fields = '__all__'
-        model = Titles
-
-
-class CategoriesSerializer(serializers.ModelSerializer):
+class UserSerializer(serializers.ModelSerializer):
 
     class Meta:
+        model = User
         fields = '__all__'
-        model = Categories
 
 
-class GenresSerializer(serializers.ModelSerializer):
+class TitleSerializer(serializers.ModelSerializer):
 
     class Meta:
+        model = Title
         fields = '__all__'
-        model = Genres
+
+
+class CategorySerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Category
+        fields = '__all__'
+
+
+class GenreSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Genre
+        fields = '__all__'
+
+
+class ReviewSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Review
+        fields = '__all__'
+
+
+class CommentSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Comment
+        fields = '__all__'
