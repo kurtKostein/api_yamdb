b0fbdb45a4b1881280b329261efbf530218ea4b8
kurtkostein <kurtkostein@yandex.ru>
2021-04-04 01:05:41 +0300

    rollback ordering in User Meta class

--- api_v1/models.py
+++ api_v1/models.py
@@ -46,6 +46,7 @@ class User(AbstractUser):
         return self.email
 
     class Meta:
+        ordering = ['id']
         verbose_name = 'Пользователь'
         verbose_name_plural = 'Пользователи'
 
